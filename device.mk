dbfd9a0e40b16e8ef61af7d017aa362a8127bb85
yarpiin <yarpiin@gmail.com>
2021-06-30 21:20:34 +0100

    umi: sakura bringup

--- device.mk
+++ device.mk
@@ -218,7 +218,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-sakura
     
 # Perf
 PRODUCT_PACKAGES += \
